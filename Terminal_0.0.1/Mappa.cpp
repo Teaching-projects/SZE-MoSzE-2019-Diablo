55fde9cbc8624ce71c7ef4a6943cbb99e51ae606
sosniki <34714702+sosniki@users.noreply.github.com>
2019-09-16 17:58:40 +0200

    Add files via upload

--- Terminal_0.0.1/Mappa.cpp
+++ Terminal_0.0.1/Mappa.cpp
@@ -1,9 +1,47 @@
 #include "Mappa.h"
 
-Mappa::Mappa()
+Mappa::Mappa(string dirName)
 {
+
 }
 
 Mappa::~Mappa()
-{
+{ }
+
+
+string Mappa::getName() {
+	return name;
+}
+
+list<string> Mappa::list() {
+	if (content.size() == 1) cout << "/" << endl;
+	string cell = "";
+	for (auto& iter : content)
+	{
+		cell += "/" + iter->getName();
+	}
+	cout << cell << endl;
+}
+
+Mappa* Mappa::search(string dirName) {
+	if (content.size() == 1) cout << "/" << endl;
+
+	for (auto& iter : content)
+	{
+		if(( iter->getName())==dirName)
+            {cout<<"siker"<<endl;
+        }
+		else return false;
+	}
+
+}
+
+bool Mappa::makeDirectory(string dirName) {
+	if (content.back()->search(dirName) != nullptr) {
+		content.back()->getName();
+		return true;
+	}
+	return false;
+
+
 }
