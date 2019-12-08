8ae0e3ea94ee355c47f65a92ef02a815728bf35f
sosniki <34714702+sosniki@users.noreply.github.com>
2019-12-08 13:23:51 +0100

    Add files via upload

---
+++ File.cpp
@@ -0,0 +1,14 @@
+#include "File.h"
+
+File::File(string fName, string content) : name(fName), content(content) {}
+
+File::~File() {}
+
+string File::getName() const {
+	return name;
+}
+
+void File::setContent(string text)
+{
+	content = text;
+}
---
+++ File.h
@@ -0,0 +1,20 @@
+#ifndef TOUCH_H
+#define TOUCH_H
+#include <string>
+
+using  namespace std;
+
+class File
+{
+private:
+	string name;
+	string content;
+
+public:
+	File(string fName, string content = "");
+	~File();
+	string getName() const;
+	void setContent(string text);
+};
+
+#endif
