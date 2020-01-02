b2609377bf01ee616a61f6a58c4856dcc184a587
adipapp <adipapp98@gmail.com>
2020-01-02 16:45:27 +0100

    File osztaly dokumentalasa

--- File.h
+++ File.h
@@ -4,16 +4,39 @@
 
 using  namespace std;
 
+/**
+@brief: fájlokat megvalósító osztály
+*/
+
 class File
 {
 private:
+	/**
+	@brief: fájlnév
+	*/
     string name;
+	/**
+	@brief: fájl tartalma
+	*/
     string content;
 
 public:
+	/**
+	@brief: fájl osztály konstruktora
+	@param fName: fájl neve, kötelezõ
+	@param content: fájl tartalma egyetlen karakterláncban, nem kötelezõ
+	*/
     File(string fName, string content = "");
     ~File();
+	/**
+	@brief: getter fgv. a fájl nevéhez
+	@return: fájl neve
+	*/
     string getName() const;
+	/**
+	@brief: setter fgv. a fájl tartalmához
+	@param text: fájl tartalma egyetlen karakterláncban
+	*/
 	void setContent(string text);
 };
 
